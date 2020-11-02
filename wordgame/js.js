5c12cd0522b2c88429455740a52a8adc02f04f04
Teemu <teemu.heittola@live.com>
2020-11-02 08:44:28 +0200

    Update js.js

--- wordgame/js.js
+++ wordgame/js.js
@@ -7,7 +7,7 @@ keySpan()
 window.addEventListener("keydown", e)
 
 function keySpan(){
-    while(theWord.length > i){
+    while(theGameWord.length > i){
        textbox.innerHTML += `<span>${theGameWord.charAt(i)}</span>`
     
     i++
@@ -16,22 +16,17 @@ function keySpan(){
 
 let a = 0
 function e(keyPressed){
-console.log(keyPressed.key)
 if(keyPressed.key == theGameWord.charAt(0)){
     theGameWord = theGameWord.slice(1)
     textbox.children[a].classList.add("used")
-    console.log("Next is: " + theGameWord.charAt(0))
 a++
 if(theGameWord == ""){
     let b = 0
     while(textbox.children.length>b){
-        console.log("removed " + b)
         textbox.children[b].classList.remove("used")       
         b++
     }
-    console.log("done")
     theGameWord = theWord
-    console.log("Reset the gameword to: " + theGameWord)
     a = 0
 }
 }
